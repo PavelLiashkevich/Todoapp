0c854ec3f7745a375bf8ab2fb484db5bab15fb90
Pavel Liashkevich <paul8paul8@mail.ru>
2022-08-10 20:23:28 +0300

    fix

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const jsonServer = require('json-server');
 const server = jsonServer.create();
 const router = jsonServer.router('./src/Assets/db.json');
 const middlewares = jsonServer.defaults({
-	statis: './build',
+	static: './build',
 });
 
 const PORT = process.env.PORT || 3000;
