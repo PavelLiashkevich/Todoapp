7cad1c9a8dfbc93eada05bdd33830c67eeab4b06
Pavel Liashkevich <paul8paul8@mail.ru>
2022-08-11 12:03:55 +0300

    fix

--- client/src/api.js
+++ client/src/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const baseURL =
-	process.env.REACT_APP_SERVER_API ||
+	// process.env.REACT_APP_SERVER_API ||
 	'https://todo-app-infotecs.herokuapp.com:3000';
 const api = axios.create({ baseURL });
 
